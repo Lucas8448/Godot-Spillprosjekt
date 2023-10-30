6d1db30e1396bf47295c10d21f7966355dfee3e1
Lucas Bateson <lucas.bateson@gmail.com>
2023-10-30 22:35:27 +0100

    Tidy up

--- Menu/main_menu.gd
+++ Menu/main_menu.gd
@@ -1,20 +1,15 @@
 extends Node2D
 
-
-export var mainGameScene : PackedScene
-
+var game = "res://Main.tscn"
 
 func _on_play_now_button_up():
-	get_tree().change_scene(mainGameScene.resource_path)
-
+	get_tree().change_scene_to_file(game)
 
 func _on_exit_game_button_down():
 	get_tree().quit()
 
-	
-
 func _on_fullscreen_toggled(button_pressed):
 	if button_pressed == true:
-			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 	else:
-			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
