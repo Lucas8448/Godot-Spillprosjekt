8eeb90b59303a6b45afb1f1f394b3804099f0818
Lucas Bateson <lucas.bateson@gmail.com>
2023-11-15 21:48:06 +0100

    Fix preloaded scenes to minimize wait

--- Menu/main_menu.gd
+++ Menu/main_menu.gd
@@ -1,10 +1,33 @@
 extends Node2D
 
-var loader = "res://Loading/Loading.tscn"
+var loader_path = "res://Loading/Loading.tscn"
 var controls = "res://Menu/Controls.tscn"
+var loading_thread = Thread.new()
+var loader_instance = null
+var path_to_load = ""  # Member variable for the path
+
+func _ready():
+	path_to_load = loader_path  # Set the path
+	var callable = Callable(self, "_load_loader_scene_in_background")  # Create a Callable
+	loading_thread.start(callable)  # Start the thread with the Callable
+
+func _load_loader_scene_in_background():
+	var scene = load(path_to_load)  # Use the member variable for the path
+	loader_instance = scene.instantiate()
 
 func _on_play_now_button_up():
-	get_tree().change_scene_to_file(loader)
+	if loader_instance:
+		var current_scene = get_tree().current_scene
+		if current_scene:
+			current_scene.queue_free()
+
+		get_tree().root.add_child(loader_instance)
+		get_tree().current_scene = loader_instance
+	else:
+		print("Loader scene is still loading.")
+
+func _exit_tree():
+	loading_thread.wait_to_finish()
 
 func _on_exit_game_button_down():
 	get_tree().quit()
